8664079868ca0908e0b3efc4f4403c522b14f21b
dilan577 <leyvadilan63@gmail.com>
2025-05-29 14:47:10 -0500

    habilitar admin

--- admin/habilitar_inhabilitar.php
+++ admin/habilitar_inhabilitar.php
@@ -78,7 +78,7 @@ $conexion->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Habilitar Inhabilitar Usuario</title>
     <link rel="icon" href="../img/Logotipo_Datasena.png" type="image/x-icon">
-    <link rel="stylesheet" href="../css/SU_admin/menu_SU_admin/habilitar_inhabilitar_SU.css">
+    <link rel="stylesheet" href="../css/SU_admin/menu_SU_admin/habilitar_in.css">
 </head>
 <body>
     <h1>DATASENA</h1>
