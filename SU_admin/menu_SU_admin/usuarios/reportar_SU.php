642f4dcdafc8f1a2d45918c653ad9c2918ed855f
Infinity-ASA <alejosalazar2206@gmail.com>
2025-07-01 18:43:40 -0500

    Cambio separacion de CSS reportar Superadmin.

--- SU_admin/menu_SU_admin/usuarios/reportar_SU.php
+++ SU_admin/menu_SU_admin/usuarios/reportar_SU.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['buscar'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Reporte</title>
     <link rel="icon" href="../../../img/Logotipo_Datasena.png" type="image/x-icon" />
-    <link rel="stylesheet" href="../../../css/admin/style.css" />
+    <link rel="stylesheet" href="../../../css/SU_admin/menu_SU_admin/style_reportar.css"/>
 </head>
 <body>
         <!-- Línea azul superior con logo del gobierno -->
